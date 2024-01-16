3c360dd3df29fa5d163b6f69325a3968c1715f76
EO <eo.code.wd@gmail.com>
2024-01-16 19:54:29 +0100

    auth routes user context profil

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,70 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
+
+const API_URL = "http://localhost:5005";
+
+function Login() {
+  const navigate = useNavigate();
+  const { authenticateUser } = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleEmail = (event) => setEmail(event.target.value);
+  const handlePassword = (event) => setPassword(event.target.value);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    const requestBody = { email, password };
+    axios
+      .post(`${API_URL}/auth/login`, requestBody)
+      .then(({ data }) => {
+        console.log("response from login", data);
+        localStorage.setItem("authToken", data.token);
+        return authenticateUser().then(() => {
+          navigate("/profile");
+        });
+      })
+      .catch((error) => {
+        setErrorMessage(error.response.data.errorMessage);
+      });
+  };
+
+  return (
+    <div>
+      <h2>login</h2>
+
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email:</label>
+        <input
+          required
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmail}
+        />
+        <br></br>
+        <br></br>
+
+        <label htmlFor="password">Password:</label>
+        <input
+          required
+          id="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">LOGIN</button>
+      </form>
+
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+    </div>
+  );
+}
+export default Login;
---
+++ src/components/user/Profile.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { Link } from "react-router-dom";
+
+
+export default function Profile() {
+  const { user, handleLogout } = useContext(UserContext);
+  console.log(user);
+  return (
+    <div>
+      <p>Welcome!</p>
+      <h1>{user && user.username}'s Profile</h1>
+      <Link to="/createTodo">Create To-Do</Link>
+      <button onClick={handleLogout}>Logout</button>
+     
+      
+    </div>
+  );
+}
